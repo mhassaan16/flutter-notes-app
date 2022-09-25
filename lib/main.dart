83fd74dc1cad72d08fce2f1f54ea6e024b0ed29e
Muhammad Hassaan <goldenbhai102@gmail.com>
2022-09-25 17:06:04 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      title: "Flutter App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print("You are verified");
+              } else {
+                print("Please verify your email");
+              }
+              return const Text("Done");
+            default:
+              return const Center(
+                child: Text("Loading..."),
+              );
+          }
+        },
+      ),
+    );
+  }
+}
